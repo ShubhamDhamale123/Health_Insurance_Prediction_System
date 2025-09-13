b5f69dce82741ce0b5b8f0af6b898fa99976855a
Shubham Dhamale <dhamaleshubham775@gmail.com>
2025-09-13 12:04:39 +0530

    Health_Insurance

---
+++ Health_Insurance/config.py
@@ -0,0 +1,12 @@
+#CSA_FILE_PATH="enter_path"
+#PORT_NUMBER=5005
+
+import os
+model_file_path=os.path.join("project_app","Health_Insurance_Model.pkl")
+json_file_path=os.path.join("project_app","project_data.json")
+
+
+
+file_path=os.path.join("project_app","linear.pkl")
+iris_path=os.path.join("project_app","iris_model.pkl")
+
